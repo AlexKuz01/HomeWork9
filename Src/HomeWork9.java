6f62f5674e23835c445a6bf5d1afae4bc5f9d06c
AlexKuz01 <oleksandr_kuznietsov@ukr.net>
2023-06-11 15:37:29 +0300

    commit2

--- Src/HomeWork9.java
+++ Src/HomeWork9.java
@@ -1,22 +1,13 @@
 
-import java.util.Arrays;
 
 public class HomeWork9 {
     public static void main(String[] args) {
-        int[] shuttles = new int[100];
-        int a;
-        int b;
-        int c = 0;
-        for (int i = 0, j = 1; j <= shuttles.length+c; j++) {
-            a = j % 10;
-            b = j / 10;
-            if (a != 4 && b != 4 && a != 9 && b != 9) {
-                shuttles[i++] = j;
-            }else {
-                c++;
+        for (int shuttleNumber = 1; shuttleNumber < 100; shuttleNumber++) {
+            if (shuttleNumber % 10 != 4 && shuttleNumber % 10 != 9 && shuttleNumber / 10 != 4 && shuttleNumber / 10 != 9) {
+                System.out.print(shuttleNumber + " ");
             }
         }
 
-        System.out.println("Номера шатлов " + Arrays.toString(shuttles));
+
     }
 }
