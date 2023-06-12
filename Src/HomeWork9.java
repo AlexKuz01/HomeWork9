9b534d93690a32bd7e1f3af5a3b19933256ae510
AlexKuz01 <oleksandr_kuznietsov@ukr.net>
2023-06-12 17:32:43 +0300

    commit3

--- Src/HomeWork9.java
+++ Src/HomeWork9.java
@@ -2,12 +2,21 @@
 
 public class HomeWork9 {
     public static void main(String[] args) {
-        for (int shuttleNumber = 1; shuttleNumber < 100; shuttleNumber++) {
-            if (shuttleNumber % 10 != 4 && shuttleNumber % 10 != 9 && shuttleNumber / 10 != 4 && shuttleNumber / 10 != 9) {
-                System.out.print(shuttleNumber + " ");
-            }
-        }
+        int shuttleNumber = 0;
+        for (int i = 0; i < 100; i++) {
+            int temp = shuttleNumber;
+            shuttleNumber++;
+            if (shuttleNumber % 10 == 4 || shuttleNumber % 10 == 9) {
+                shuttleNumber++;
 
+            }
+
+            if ((shuttleNumber / 10) % 10 == 4 || (shuttleNumber / 10) % 10 == 9) {
+                shuttleNumber = temp + 12;
+
+            }
+            System.out.print(shuttleNumber + " ");
+        }
 
     }
 }
